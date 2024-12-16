161a9e7da7023402c1e333ee49aa69f3392a2af6
davif <davif@DAVI>
2024-12-15 20:10:48 -0400

    Adicionar arquivos de projeto.

---
+++ NuvemManageSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using NuvemManageSystem.SupportFunctions;
+
+class Program
+{
+    static void Main()
+    {
+        InputFunctions input = new InputFunctions();
+        Console.WriteLine("--------------{Nuvem Manage System}--------------");
+        string? opt = input.InputString(false, "Teste: ", "Caracter");
+        Console.WriteLine($"Você digitou: {opt}");
+        Console.ReadLine();
+    }
+}
---
+++ NuvemManageSystem/SupportFunctions/InputFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuvemManageSystem.SupportFunctions
+{
+    internal class InputFunctions
+    {
+        string? Response = null;
+        public string? InputString(bool _null, string text, string title)
+        {
+            Console.WriteLine(text + _null);
+            Response = Console.ReadLine();
+            if (_null == false && Response == null || Response == "")
+            {
+                InputNotNull(text, title);
+            }
+
+            return Response;
+        }
+
+        private void InputNotNull(string text, string title)
+        {
+            while (Response == null || Response == "")
+            {
+                Console.WriteLine($"Digite um {title} válido: ");
+                Response = Console.ReadLine();
+            }
+        }
+    }
+}
