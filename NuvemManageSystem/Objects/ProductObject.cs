e43055b89a7b9302bade3dada3adfd81528cdcaa
davi <davifelex.programador@gmail.com>
2024-12-22 11:00:33 -0400

    Add appsettings.json

---
+++ NuvemManageSystem/Objects/ProductObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuvemManageSystem.Objects
+{
+    internal class ProductObject
+    {
+
+    }
+}
