78e518b710960a839469c7f25179fc156ef6fa07
davi <davifelex.programador@gmail.com>
2024-12-23 21:40:42 -0400

    Add WhereObject

---
+++ NuvemManageSystem/Objects/WhereObject.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuvemManageSystem.Objects
+{
+    internal class WhereObject
+    {
+        private Dictionary<string, Condition> Conditions = new Dictionary<string, Condition>();
+        private Dictionary<string, string> Connections = new Dictionary<string, string>();
+        private Condition TempCondition = new Condition("", "", "");
+        private string WhereResult = "";
+        private Dictionary<string, string> Parameters = new Dictionary<string, string>();
+
+        public bool AddCondition(string column, string value, string relation, string Connection)
+        {
+            try
+            {
+                TempCondition.Column = column;
+                TempCondition.Value = value;
+                TempCondition.Relation = relation;
+                Conditions.Add(column, TempCondition);
+                Connections.Add(column, Connection);
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine($"Ocorreu um erro ao adicionar condição: {ex.Message}");
+                return false;
+            }
+            return true;
+        }
+
+        public bool RemoveCondition(string column)
+        {
+            try
+            {
+                Conditions.Remove(column);
+                Connections.Remove(column);
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine ($"Ocorreu um erro ao tentar remover condição: {ex.Message}");
+                return false;
+            }
+            return true;
+        }
+
+        public bool BuildWhere()
+        {
+            try
+            {
+                foreach (string conditionKey in Conditions.Keys)
+                {
+                    WhereResult += $"{Connections[conditionKey]} {conditionKey} = @{conditionKey} ";
+                    Parameters[$"@{conditionKey}"] = Conditions[conditionKey].Value;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro ao tentar criar o Where:{ex.Message}");
+                return false;
+            }
+            Conditions.Clear();
+            Connections.Clear();
+            Parameters.Clear();
+            return true;
+        }
+        public string GetWhere()
+        {
+            if (Conditions.Count > 0)
+            {
+                BuildWhere();
+            }
+            return WhereResult;
+        }
+        public Dictionary<string, string> GetParameters()
+        {
+            if (Parameters.Count > 0)
+            {
+                BuildWhere();
+            }
+            return Parameters;
+        }
+
+    }
+}
+
+struct Condition
+{
+    public string Column;
+    public string Value;
+    public string Relation;
+    public Condition(string Column, string Value, string Relation)
+    {
+        this.Column = Column;
+        this.Value = Value;
+        this.Relation = Relation;
+    }
+}
--- NuvemManageSystem/SupportFunctions/DatabaseManager.cs
+++ NuvemManageSystem/SupportFunctions/DatabaseManager.cs
@@ -1,12 +1,148 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using NuvemManageSystem.Objects;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
 
 namespace NuvemManageSystem.SupportFunctions
 {
-    internal class DatabaseManager
+    public class DatabaseManager
     {
+        private readonly string? _connectionString;
+
+        public DatabaseManager()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        }
+
+        public SqlConnection OpenConnection()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(_connectionString);
+                connection.Open();
+                Console.WriteLine("Connection ok");
+                return connection;
+            }
+            catch (Exception ex) 
+            { 
+                Console.WriteLine($"Error in open connection: {ex.Message}");
+                throw;
+            }
+        }
+
+        public void ExecuteCommand(string sql)
+        {
+            using (SqlConnection connection = OpenConnection())
+            {
+                try
+                {
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    int affectedRows = command.ExecuteNonQuery();
+                    Console.WriteLine($"Comando executado com sucesso, linhas afetadas: {affectedRows}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erro ao tentar executar comando sql: {ex.Message}");
+                }
+            }
+        }
+
+        public DataTable? ExecuteQuery(string sql)
+        {
+            using (SqlConnection connection = OpenConnection())
+            {
+                try
+                {
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+                    return dataTable;
+                }
+                catch (Exception ex)
+                { 
+                    Console.WriteLine($"Erro ao tentar executar a consulta: {ex.Message}");
+                    return null;
+                }
+            }
+        }
+
+        public void ExecuteInsert(string table, string[] columns, string[] values)
+        {
+            if (columns.Length != values.Length)
+            {
+                Console.WriteLine("Erro, para cada coluna deve aver um valor");
+                return;
+            }
+            try
+            {
+                using (SqlConnection connection = OpenConnection())
+                {
+                    string columnsList = string.Join(", ", columns);
+                    string parametersList = string.Join(", ", columns.Select((col, index) => "@" + col));
+
+                    string sql = $"INSERT INTO {table} ({columnsList}) VALUES ({parametersList})";
+
+                    SqlCommand command = new SqlCommand(sql, connection);
+
+                    for (int i = 0; i < columns.Length; i++)
+                    {
+                        command.Parameters.AddWithValue("@" + columns[i], values[i]);
+                    }
+
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    Console.WriteLine($"Inserção realizada com sucesso! Linhas afetadas: {rowsAffected}");
+                }
+            }
+            catch (Exception ex )
+            {
+                Console.WriteLine($"Erro ao tentar realizar um INSERT: {ex.Message}");
+            }
+        }
+
+        private bool ExecuteSqlWriter(string typeCommand, string table, string[] columns, string[] values, WhereObject? Where=null)
+        {
+            if (columns.Length != values.Length)
+            {
+                Console.WriteLine($"Quantidade de valores diferente das colunas:\nColunas: {columns.Length}\nValores: {values.Length}");
+                return false;
+            }
+            string sql = "";
+            string columnsList = string.Join(", ", columns);
+            string parametersList = string.Join(", ", columns.Select((col, index) => "@" + col));
+
+            switch (typeCommand.ToLower())
+            {
+                case "insert":
+                    sql = $"INSERT INTO {table} ({columnsList}) VALUES ({parametersList})";
+                    break;
+                case "update":
+                    sql = $"UPDATE {table}";
+                    break;
+                case "delete":
+                    break;
+
+                default:
+                    Console.WriteLine("TypeCommand invalido");
+                    return false;
+            }
+            if (Where != null)
+            {
+                sql += " WHERE ";
+                sql += Where.GetWhere();
+            }
+
+
+            return true;
+        }
     }
 }
